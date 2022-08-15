f661747f0a17775c8e4fd79fae0c33425472961c
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-15 20:43:42 +0600

    added index.html, login.html into 'templates'folder

--- basic_auth_project/basic_auth_app/views.py
+++ basic_auth_project/basic_auth_app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is HOME page")
+    return render(request, "index.html")
+def login(request):
+    return render(request, "login.html")
